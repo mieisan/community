e02f8873e45ad046a361f82a9ef0808419a8568e
mieisan <longtoooooog@gmail.com>
2026-07-09 15:15:54 +0900

    aaa

--- community/settings.py
+++ community/settings.py
@@ -46,7 +46,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q3tdko+9l&cqec-@j25lukdwx=%g0c86h+k)5w$+096ptre4&l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
