7018fbfdd8b8c44507e824ffa24492b8bcf056ed
mieisan <longtoooooog@gmail.com>
2026-07-09 16:56:24 +0900

    aa

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
 pip install -r requirements.txt
-
 python manage.py collectstatic --noinput
-
 python manage.py migrate
+
+DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD" python manage.py createsuperuser --noinput --username admin --email admin@example.com || true
