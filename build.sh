45178f523c91a175f3c9e6f73cf0bf8cb7df6246
mieisan <longtoooooog@gmail.com>
2026-07-09 17:18:11 +0900

    q

--- build.sh
+++ build.sh
@@ -3,4 +3,20 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-DJANGO_SUPERUSER_PASSWORD="$DJANGO_SUPERUSER_PASSWORD" python manage.py createsuperuser --noinput --username admin --email admin@example.com || true
+python manage.py shell << 'EOF'
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+username = "siteadmin"
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+email = "admin@example.com"
+
+user, created = User.objects.get_or_create(username=username, defaults={"email": email})
+user.set_password(password)
+user.is_staff = True
+user.is_superuser = True
+user.save()
+
+print("created new user" if created else "updated existing user", username)
+EOF
