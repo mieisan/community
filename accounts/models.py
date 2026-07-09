b699a9f17278a0fa92dff547d55e6bb4f183368f
mieisan <longtoooooog@gmail.com>
2026-07-09 13:03:01 +0900

    discordfix

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     icon = models.ImageField(upload_to="profile_icons/", blank=True, null=True)
+    icon_url = models.URLField(blank=True)   # ←追加
 
     def __str__(self):
         return self.user.username
